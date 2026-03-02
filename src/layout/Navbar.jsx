169318fa775fdb1bb4039a5cf7e4c10763063468
Thach <Thachvu1127@gmail.com>
2026-03-02 15:51:19 -0800

    navbar component now handles mobile navigation, add fade-in animation for mobile development

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@/components/Button.jsx";
+import { Menu, X } from "lucide-react";
 
 const navLinks = [
   { href: "#about", label: "About" },
@@ -8,6 +9,7 @@ const navLinks = [
   { href: "#testimonials", label: "Testimonials" },
 ];
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
       <nav
@@ -44,6 +46,47 @@ const Navbar = () => {
         </div>
 
         {/*  Mobile menu button */}
+        <button className={"md:hidden p-2 text-foreground "}>
+          {menuOpen ? (
+            <X
+              onClick={() => setMenuOpen(!menuOpen)}
+              className={"hover:text-primary cursor-pointer"}
+            />
+          ) : (
+            <Menu
+              onClick={() => setMenuOpen(!menuOpen)}
+              className={"hover:text-primary cursor-pointer"}
+            ></Menu>
+          )}
+
+          {menuOpen && (
+            <div
+              className={
+                "md:hidden fixed top-0 right-0 h-full w-screen my-15 animate-fade-in"
+              }
+            >
+              <div
+                className={
+                  "glass-strong container mx-auto px-6 py-6 flex flex-col items-center gap-4"
+                }
+              >
+                {navLinks.map((link) => (
+                  <a
+                    href={link.href}
+                    key={link.label}
+                    className={
+                      "text-lg text-muted-foreground hover:text-foreground py-2 px-15 rounded-full hover:bg-surface"
+                    }
+                  >
+                    {link.label}
+                  </a>
+                ))}
+
+                <Button btnSize={"sm"}>Contact Me</Button>
+              </div>
+            </div>
+          )}
+        </button>
       </nav>
     </header>
   );
